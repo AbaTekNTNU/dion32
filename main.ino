543fe6cf2adb111c08354592c753ce6a6855684d
Kristian <kristianint@outlook.com>
2021-03-01 18:45:53 +0100

    more error animations and increase num leds to 5m

--- main.ino
+++ main.ino
@@ -1,45 +1,43 @@
 #define UIPETHERNET_DEBUG_UDP
 
-#define min(a,b) ((a)<(b)?(a):(b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
 //#define WIFI
 #ifdef WIFI
 
-    #define ARTNET_ENABLE_WIFI
-    #ifdef VSCODE
-    #define ESP_PLATFORM // tell Artnet.h we are using esp32 and include correct wifi library
-    #endif
+#define ARTNET_ENABLE_WIFI
+#ifdef VSCODE
+#define ESP_PLATFORM // tell Artnet.h we are using esp32 and include correct wifi library
+#endif
 
 #else
 
-    #include <EthernetENC.h>
-    #include <SPI.h>
-    #undef ESP_PLATFORM
-    #define ARTNET_ENABLE_ETHER
+#include <EthernetENC.h>
+#include <SPI.h>
+#undef ESP_PLATFORM
+#define ARTNET_ENABLE_ETHER
 
 #endif
 
 #include "artnet/ArtNet.h"
 #include <Adafruit_NeoPixel.h>
-#include "utils.h"
-
 
 #ifdef VSCODE // when building arduino, all ino files are automatically included, this is just a fix for vs code
-    #include "mode_raw.ino"
-    #include "wifi-creds.ino"
+#include "mode_raw.ino"
+#include "utils.ino"
+#include "wifi-creds.ino"
 #endif
 
 // Config Options
 // Rename "wifi-creds.ino-example" to "wifi-creds.ino" and add your credentials into the updateWifiCreds() function
-char* ssid;
-char* password;
+char *ssid;
+char *password;
 
 IPAddress ip(192, 168, 1, 5);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 const int startUniverse = 0;
-const int numLeds = 120;
-
+const int numLeds = 360;
 
 // static
 const byte dataPin = 13;
@@ -54,7 +52,8 @@ ArtnetWiFiReceiver artnet;
 ArtnetReceiver artnet;
 #endif
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
 
     Serial.println(" - Starting....");
@@ -67,7 +66,8 @@ void setup() {
     Serial.println(" - WiFi: Configuring IPv4...");
     WiFi.config(ip, gateway, subnet);
     Serial.println(" - WiFi: Checking status");
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
         Serial.print(".");
         delay(500);
     }
@@ -75,23 +75,29 @@ void setup() {
     Serial.println(WiFi.localIP());
 #else
 
-    byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+    byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
     Ethernet.init(5);
     Serial.println(" - Ethernet: Beginning...");
     Ethernet.begin(mac, ip);
     Serial.println(" - Ethernet: Checking HW status....");
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    if (Ethernet.hardwareStatus() == EthernetNoHardware)
+    {
         Serial.println("Ethernet shield was not found. Sorry, can't run without hardware. :(");
-        while (true) {
+
+        while (true)
+        {
             delay(1); // do nothing, no point running without Ethernet hardware
         }
     }
     Serial.println(" - Ethernet: Checking Link status....");
-    if (Ethernet.linkStatus() == LinkOFF) {
+    if (Ethernet.linkStatus() == LinkOFF)
+    {
         delay(500);
-        if (Ethernet.linkStatus() == LinkOFF) {
+        if (Ethernet.linkStatus() == LinkOFF)
+        {
             Serial.println("Ethernet cable is not connected.");
-            while (true) {
+            while (true)
+            {
                 delay(1); // do nothing, no point running without Ethernet hardware
             }
         }
@@ -115,21 +121,23 @@ void setup() {
 
     Serial.println(" - Leds: Success, playing animation....");
 
-    initTest();
+    playSuccessAnimation();
 }
 
-void loop() {
-    artnet.parse();  // check if artnet packet has come and execute callback
+void loop()
+{
+    artnet.parse(); // check if artnet packet has come and execute callback
 }
 
 const byte MODE_RAW = 255;
 
-void dmx(const uint32_t universe, const byte* data, const uint16_t length) {
+void dmx(const uint32_t universe, const byte *data, const uint16_t length)
+{
     Serial.println("Running dmx callback");
 
-#ifndef WIFI 
+#ifndef WIFI
     // The EthernetENC library seems to have a problem with UDP sockets.
-    // When the first message from the (ip, port) pair is received the 
+    // When the first message from the (ip, port) pair is received the
     // socket gets locked to only receiving packets from that (ip, port).
     // By restarting the socket we can start receiving from other (ip, port)s.
     Serial.println("Restarting ArtNet socket...");
@@ -140,23 +148,27 @@ void dmx(const uint32_t universe, const byte* data, const uint16_t length) {
 
     int numBlobs = data[0];
     byte blobSize = 9;
-    if (numBlobs == MODE_RAW) {
+    if (numBlobs == MODE_RAW)
+    {
         raw_mode(universe, data + 1, length - 1);
         return;
     }
-    for(int i = 0; i < length; i += 1) {
+    for (int i = 0; i < length; i += 1)
+    {
         Serial.print("i: ");
         Serial.print(i);
         Serial.print(", value: ");
         Serial.println(data[i]);
     }
-    uint16_t l = ((length-1) / blobSize) * blobSize;
-    if (l > numBlobs * blobSize) {
+    uint16_t l = ((length - 1) / blobSize) * blobSize;
+    if (l > numBlobs * blobSize)
+    {
         l = numBlobs * blobSize;
     }
     leds.clear();
     leds2.clear();
-    for(int i = 0; i < l; i += blobSize) {
+    for (int i = 0; i < l; i += blobSize)
+    {
         Serial.print("\nRunning data i: ");
         Serial.print(i);
         Serial.print(", of length: ");
@@ -175,8 +187,9 @@ const byte MODE_FADE_OUT_MIDDLE = 3;
 const byte MODE_FLAME = 4;
 const byte MODE_SPARKLE = 5;
 
-void draw(const byte* data) {
-    const uint16_t x = (data[0]<<8) | data[1];
+void draw(const byte *data)
+{
+    const uint16_t x = (data[0] << 8) | data[1];
     const uint16_t width = (data[2] << 8) | data[3];
     const byte r = data[4];
     const byte g = data[5];
@@ -184,23 +197,25 @@ void draw(const byte* data) {
     const byte mode = data[7];
     const byte mode_data = data[8];
 
-    for(uint16_t i = x; i < x + width; i += 1) {
-        addColor(i, r, g, b, ((float) i) / ((float) width));      
+    for (uint16_t i = x; i < x + width; i += 1)
+    {
+        addColor(i, r, g, b, ((float)i) / ((float)width));
     }
 }
 
-void addColor(uint16_t n, byte r, byte g, byte b, float opacity) {
+void addColor(uint16_t n, byte r, byte g, byte b, float opacity)
+{
     uint32_t cur = leds.getPixelColor(n);
 
     uint16_t old_r = 0xff << 16 & cur;
     uint16_t old_g = 0xff << 8 & cur;
     uint16_t old_b = 0xff & cur;
 
-    r = (byte) (((float) r) * opacity);
+    r = (byte)(((float)r) * opacity);
     r = min(r + old_r, 255);
-    g = (byte) (((float) g) * opacity);
+    g = (byte)(((float)g) * opacity);
     g = min(g + old_g, 255);
-    b = (byte) (((float) b) * opacity);
+    b = (byte)(((float)b) * opacity);
     b = min(b + old_b, 255);
 
     Serial.print("n: ");
--- utils.ino
+++ utils.ino
@@ -2,7 +2,8 @@
 #include <Adafruit_NeoPixel.h>
 #endif
 
-void initTest() {
+void playSuccessAnimation()
+{
     leds.fill(leds.Color(127, 0, 0), 0, numLeds);
     leds2.fill(leds.Color(127, 0, 0), 0, numLeds);
     leds.show();
@@ -24,38 +25,86 @@ void initTest() {
     leds2.show();
 }
 
-float byte2float(byte value) {
-    return ((float) value) / (float) 255.0;
+void playEthernetModuleError()
+{
+    error(1);
 }
 
-byte float2byte(float value) {
-    return (byte) (value * 255);
+void playEthernetCableError()
+{
+    error(2);
 }
 
-int scaleToLeds(byte value) {
+void playEthernetNoIPError()
+{
+    error(3);
+}
+
+void playWifiNotConnectedError()
+{
+    error(4);
+}
+
+void error(int num)
+{
+    while (true)
+    {
+        leds.fill(leds.Color(50, 0, 0), 0, num);
+        leds2.fill(leds.Color(50, 0, 0), 0, num);
+        leds.show();
+        leds2.show();
+        delay(500);
+
+        leds.clear();
+        leds2.clear();
+        leds.show();
+        leds2.show();
+        delay(500);
+    }
+}
+
+float byte2float(byte value)
+{
+    return ((float)value) / (float)255.0;
+}
+
+byte float2byte(float value)
+{
+    return (byte)(value * 255);
+}
+
+int scaleToLeds(byte value)
+{
     return byte2float(value) * numLeds;
 }
 
-byte getWhite(uint32_t color) {
+byte getWhite(uint32_t color)
+{
     return (color >> 24) && 0xff;
 }
 
-byte getRed(uint32_t color) {
+byte getRed(uint32_t color)
+{
     return (color >> 16) && 0xff;
 }
 
-byte getGreen(uint32_t color) {
+byte getGreen(uint32_t color)
+{
     return (color >> 8) && 0xff;
 }
 
-byte getBlue(uint32_t color) {
-    return color&& 0xff;
+byte getBlue(uint32_t color)
+{
+    return color && 0xff;
 }
 
-bool isBlack(uint32_t color) {
+bool isBlack(uint32_t color)
+{
     return getRed(color) == 0 && getGreen(color) == 0 && getBlue(color) == 0;
 }
 
-int getNumColors(const uint32_t primary, const uint32_t secondary, const uint32_t tertiary){
-    return isBlack(secondary) ? 1 : isBlack(tertiary) ? 2 : 3;
+int getNumColors(const uint32_t primary, const uint32_t secondary, const uint32_t tertiary)
+{
+    return isBlack(secondary) ? 1 : isBlack(tertiary) ? 2
+                                                      : 3;
 }
